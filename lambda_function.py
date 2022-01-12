98167db2a50e9be1638b9fac9c99653b7879fa47
nakul s ananth <nakul.s.ananth@apiseroinc.onmicrosoft.com>
2022-01-12 13:49:46 +0530

    Req matplotlib

--- lambda_function.py
+++ lambda_function.py
@@ -12,7 +12,7 @@ def lambda_handler(event, context):
     s3_file_key = event['Records'][0]['s3']['object']['key'];
     bucket = 'sourcebucketdemo2';
     s3 = boto3.client('s3', aws_access_key_id='AKIAZRRIY4NWWO6V7QBS',  aws_secret_access_key='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')
-    obj = s3.get_object(Bucket=bucket, Key=s3_file_key)
+    obj = s3.get_object(Bucket=gitbucket, Key=s3_file_key)
     initial_df = pd.read_csv(io.BytesIO(obj['Body'].read()));
 
     service_name = 's3'
